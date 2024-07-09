d87314fe4bfa1eb28efc74f0bed9e311beaaff3d
Issei Naruta <naruta@ivry.jp>
2024-07-09 16:46:35 +0900

    Introduce CopilotChat.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,6 +6,20 @@ return {
     end,
   },
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    config = function()
+      require("CopilotChat").setup({})
+    end,
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+  },
+  {
     "nvim-tree/nvim-tree.lua",
     config = function()
       require("nvim-tree").setup({})
