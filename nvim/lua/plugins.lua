41aa470a498506d92c244757b689f6a16c8b83fc
Issei Naruta <naruta@ivry.jp>
2024-08-09 15:42:59 +0900

    Remove Copilot

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,25 +1,5 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    config = function()
-      require("copilot").setup({})
-    end,
-  },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    config = function()
-      require("CopilotChat").setup({})
-    end,
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-  },
-  {
     "nvim-tree/nvim-tree.lua",
     config = function()
       require("nvim-tree").setup({})
